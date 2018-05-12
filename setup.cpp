8271ff1441b962d9c11c8c08a27612ecf60c1ba2
Brent Hurst <seqvalley77@aol.com>
2018-05-11 21:21:54 -0400

    keeps track of whether earmarksamount matches amount. wont let the same acct name be entered twice on setup

--- setup.cpp
+++ setup.cpp
@@ -22,10 +22,11 @@ void Finances::SetupAddAccounts(const string& type,map<string,Account*>& m)
 	double d;
 	Account* a;
 	int junk;
+	int earmarkAmount = 0;
 
 	while(1)
 	{
-		if(!m.empty())
+		if(!m.empty() && type=="location")
 		{
 			do
 			{
@@ -37,10 +38,26 @@ void Finances::SetupAddAccounts(const string& type,map<string,Account*>& m)
 			if(c=='n' || c=='N')
 				break;
 		}
+		else if(type=="earmark" && amount==earmarkAmount)
+			break;
+		else if(type=="earmark" && earmarkAmount>amount)
+		{
+			printf("You've entered more money in earmarks than you did in accounts.\n");
+			printf("Please try again.\n");
+			exit(0);
+		}
 
-		printf("Enter a %s name: ",type.c_str());
+		printf("Enter %s name: ",type.c_str());
 		scanf("%s",s);
 		while((junk=getchar()) != '\n' && junk != EOF);
+		str = s;
+
+		if(allaccounts.find(s) != allaccounts.end())
+		{
+			printf("\"%s\" already exists as a %s.\n",s,(*allaccounts.find(str)).second->t.c_str());
+			continue;
+		}
+
 		printf("Enter the starting amount in this %s: $",type.c_str());
 		scanf("%lf",&d);
 		while((junk=getchar()) != '\n' && junk != EOF);
@@ -54,9 +71,12 @@ void Finances::SetupAddAccounts(const string& type,map<string,Account*>& m)
 
 		if(c=='y' || c=='Y')
 		{
-			str = s;
 			a = new Account(str,type);
 			a->amount = d;
+			if(type=="location")
+				amount += d;
+			else
+				earmarkAmount += d;
 			m[str] = a;
 			allaccounts[str] = a;
 			FindSuperAccount(str,a,m,type);
@@ -74,7 +94,7 @@ void Finances::Setup(const string& filename)
 
 	do
 	{
-		printf("Would you like to save changes or discard changes?");
+		printf("Would you like to save changes or discard changes?\n");
 		printf("Enter 1 for save, and enter 2 for discard: ");
 		scanf("%c",&c);
 		while((junk=getchar()) != '\n' && junk != EOF);
