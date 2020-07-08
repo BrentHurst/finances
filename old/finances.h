f6e7b28c8c50080fcfa306a6b534c851c31cb114
Brent Hurst <GBrentHurst@gmail.com>
2020-07-08 00:32:49 -0400

    Removed iodefinesandconstants.h

--- old/finances.h
+++ old/finances.h
@@ -9,6 +9,16 @@
 #include "Date.h"
 using namespace std;
 
+/* #define PutLine() PutDelimitedLine(f,c0,c1,esc,v)
+#define PutLines() PutDelimitedFile(f,c0,c1,esc,subaccounts)
+#define GetLine() GetDelimitedLine(f,c0,c1,esc,v)
+#define GetLines() GetDelimitedFile(f,c0,c1,esc,file)
+#define FlushInputBuffer() while((junk=getchar()) != '\n' && junk != EOF)
+
+const char c0 = '\0';
+const int c1 = '\n';
+const int esc = '`'; */
+
 
 class Finances
 {
